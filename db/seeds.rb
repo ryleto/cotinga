897cfb9d64bd69c8ac652582a901a6cbf107a0ee
Ryan Leto <ryjleto@gmail.com>
2015-10-13 22:04:35 +0000

    planting seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+['registered', 'base_client', 'premium_client', 'moderator', 'admin'].each do |role|
+  Role.find_or_create_by({name: role})
+end
