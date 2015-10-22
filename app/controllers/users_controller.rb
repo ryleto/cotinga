d71fa0c397465e3429e9961dfa208e9eec5f61cd
Ryan Leto <ryjleto@gmail.com>
2015-10-22 00:39:13 +0000

    controller updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
     def edit
         @user = User.find(params[:id])
     end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+          flash[:success] = "Profile updated"
+          redirect_to @user
+        else
+          render 'edit'
+        end
+    end
+    
+    private
+    
+        def user_params
+          params.require(:user).permit(:name, :company, :email, :password, :password_confirmation)
+        end
 end
