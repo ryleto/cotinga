e5d3d0727aebf8dc33a77b2355c9defad53504b7
Ryan Leto <ryjleto@gmail.com>
2015-11-06 00:21:05 +0000

    mailer updates

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,2 +1,10 @@
 class MessageMailer < ApplicationMailer
+    default from: "Cotinga <noreply@cotinga.com>"
+    default to: "Contact Name <first.last@domain.com>"
+
+    def new_message(message)
+        @message = message
+        
+        mail subject: "Message from #{message.name}"
+    end
 end
