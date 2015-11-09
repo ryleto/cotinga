7950dd837ba34d4e488db483929e0f65c95846d4
Ryan Leto <ryjleto@gmail.com>
2015-11-09 07:27:11 +0000

    mailer updates

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,10 +1,11 @@
 class MessageMailer < ApplicationMailer
     default from: "Cotinga <noreply@cotinga.com>"
-    default to: "Contact Name <first.last@domain.com>"
+    default to: "Recipient Name <recipient@gmail.com>"
 
     def new_message(message)
         @message = message
         
         mail subject: "Message from #{message.name}"
     end
+    
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message
     include ActiveModel::Model
-    attr_accessor :name, :email, :company, :subject, :content
-    validates :name, :email, :subject, :content, presence: true
+    attr_accessor :name, :email, :content
+    validates_presence_of :name, :email, :content
+    validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
