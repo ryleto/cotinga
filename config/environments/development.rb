a358670293624b4c85578e7de96267bc72b9dd97
Ryan Leto <ryjleto@gmail.com>
2015-11-11 14:34:50 +0000

    config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'gmail.com' }
   
   config.action_mailer.delivery_method = :smtp
   
