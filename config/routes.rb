f5c1bf87e447cf2b1cf539d02ff4dec476b0d631
Ryan Leto <ryjleto@gmail.com>
2015-11-26 17:01:08 +0000

    route updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ Rails.application.routes.draw do
   #devise_for :users
   #devise_for :users, :controllers => { users: 'registrations' }
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  
   # static page routes
-  root                'static_pages#home'
-  get    'about'   => 'static_pages#about'
-  get    'products'=> 'static_pages#products'
-  #get    'contact' => 'static_pages#contact'
-  get    'dashboard' => 'static_pages#dashboard'
-  get 'contact', to: 'messages#new', as: 'contact'
-  post 'contact', to: 'messages#create'
-  get 'tags/:tag', to: 'articles#index', as: :tag
+  root                   'static_pages#home'
+  get   'about'     => 'static_pages#about'
+  get   'products'  => 'static_pages#products'
+  get   'dashboard' => 'static_pages#dashboard'
+  get   'contact',  to: 'messages#new', as: 'contact'
+  post  'contact',  to: 'messages#create'
+  get   'tags/:tag',to: 'articles#index', as: :tag
   
   resources :users
   resources :articles
