afaa57467e357db1095792b0456806b11a611e9a
Ryan Leto <ryjleto@gmail.com>
2015-12-03 07:01:10 +0000

    replacing user affiliation model

--- app/models/affiliation.rb
+++
@@ -1,5 +0,0 @@
-class Affiliation < ActiveRecord::Base
-    has_many :users
-    validates :company_name, presence: true
-    validates :company_id, uniqueness: true
-end
--- db/migrate/20151115014145_create_affiliations.rb
+++
@@ -1,8 +0,0 @@
-class CreateAffiliations < ActiveRecord::Migration
-  def change
-    create_table :affiliations do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
