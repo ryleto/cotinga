edcb5453000c46895b9633bed802476ae128d589
Ryan Leto <ryjleto@gmail.com>
2015-12-28 01:37:32 +0000

    incremental changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219214409) do
+ActiveRecord::Schema.define(version: 20151228012801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -104,11 +104,11 @@ ActiveRecord::Schema.define(version: 20151219214409) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "role"
     t.string   "name"
     t.string   "username"
     t.string   "company"
     t.boolean  "admin"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
