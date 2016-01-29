9a8208537372c5b610b4cb0896a5f668b797d3f6
Ryan Leto <ryjleto@gmail.com>
2016-01-29 23:40:48 +0000

    fixing some issues

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :commentable, polymorphic: true
   validates :content, presence: true
-  default_scope -> { order(pubdate: :desc) }
+  default_scope -> { order(created_at: :desc) }
 end
