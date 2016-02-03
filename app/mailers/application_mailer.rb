737b7acdc900414e6ae12534b32f07fcfb6d1e95
Ryan Leto <ryjleto@gmail.com>
2016-02-03 05:46:35 +0000

    mailer changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
-  layout 'mailer'
+  default from: "Cotinga App <noreply@cotinga.com>"
+  default to: "inbox@cotinga.com"
+  append_view_path Rails.root.join('app', 'views', 'mailers')
 end
--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,7 +1,4 @@
 class MessageMailer < ApplicationMailer
-    default from: "Cotinga <noreply@cotinga.com>"
-    default to: "Recipient Name <recipient@gmail.com>"
-
     def new_message(message)
         @message = message
         mail subject: "Message from #{message.name}"
